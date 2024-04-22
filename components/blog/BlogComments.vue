4620b2886765e9ae47f09eb49142f321697a36b0
hcavarsan <hcavarsan@yahoo.com.br>
2024-04-22 14:12:30 -0300

    feat: change utterance widget to giscus

--- components/blog/BlogComments.vue
+++ components/blog/BlogComments.vue
@@ -9,22 +9,23 @@
 export default {
   mounted() {
     const scriptTag = document.createElement('SCRIPT')
-    scriptTag.setAttribute('src', 'https://utteranc.es/client.js')
-    scriptTag.setAttribute('repo', 'hcavarsan/kftray-blog')
-    scriptTag.setAttribute('issue-term', 'pathname')
-    scriptTag.setAttribute('label', 'blog')
-    scriptTag.setAttribute('theme', 'github-dark')
-    scriptTag.setAttribute('crossorigin', 'anonymous')
+    scriptTag.setAttribute('src', 'https://giscus.app/client.js')
+	scriptTag.setAttribute('data-repo', 'hcavarsan/kftray-blog')
+	scriptTag.setAttribute('data-repo-id', 'R_kgDOLw9pEw')
+	scriptTag.setAttribute('data-category', 'General')
+	scriptTag.setAttribute('data-category-id', 'DIC_kwDOLw9pE84Ce3Af')
+	scriptTag.setAttribute('data-mapping', 'pathname')
+	scriptTag.setAttribute('data-strict', '0')
+	scriptTag.setAttribute('data-reactions-enabled', '1')
+	scriptTag.setAttribute('data-emit-metadata', '0')
+	scriptTag.setAttribute('data-input-position', 'top')
+	scriptTag.setAttribute('data-theme', 'noborder_dark')
+	scriptTag.setAttribute('data-lang', 'en')
+	scriptTag.setAttribute('data-loading', 'lazy')
+	scriptTag.setAttribute('crossorigin', 'anonymous')
     scriptTag.setAttribute('async', 'async')
     this.$el.append(scriptTag)
   },
 }
 </script>
 
-<style>
-.utterances {
-  width: 100%;
-  margin: 0 !important;
-  max-width: 100%;
-}
-</style>
