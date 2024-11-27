5a02b098adf27d216555b65ade37788aaa3a2f94
hcavarsan <hcavarsan@yahoo.com.br>
2024-11-27 15:55:07 -0300

    chore: bump version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,132 +1,132 @@
 export default defineNuxtConfig({
-	extends: "@nuxt-themes/docus",
-	modules: ["@nuxtjs/tailwindcss", "@nuxtjs/plausible", "@nuxt/content"],
-	css: ["@fortawesome/fontawesome-svg-core/styles.css", "~/assets/css/global.css"],
+				extends: "@nuxt-themes/docus",
+				modules: ["@nuxtjs/tailwindcss", "@nuxtjs/plausible", "@nuxt/content", '@nuxtjs/robots'],
+				css: ["@fortawesome/fontawesome-svg-core/styles.css", "~/assets/css/global.css"],
 
-	app: {
-	  head: {
-		htmlAttrs: {
-		  class: 'loading'
-		}
-	  }
-	},
+				app: {
+						head: {
+								htmlAttrs: {
+										class: 'loading'
+								}
+						}
+				},
 
-	vite: {
-	  css: {
-		devSourcemap: true
-	  },
-	  optimizeDeps: {
-		include: ['@nuxt-themes/docus'],
-		exclude: ['fsevents', 'globby', 'unicorn-magic']
-	  },
-	  build: {
-		chunkSizeWarningLimit: 1000,
-		cssCodeSplit: false,
-		minify: "terser",
-		terserOptions: {
-		  compress: {
-			drop_console: true,
-			drop_debugger: true,
-		  },
-		},
-	  },
-	  ssr: {
-		noExternal: [
-		  'fsevents',
-		  'globby',
-		  'unicorn-magic',
-		  '@nuxt/content',
-		  '@nuxt-themes/docus',
-		  'nuxt',
-		  '#imports',
-		  '#internal/nitro',
-		  '#internal/nuxt'
-		]
-	  }
-	},
-	components: {
-		dirs: [
-		  {
-			path: '~/components/content',
-			global: true,
-			pathPrefix: false
-		  },
-		  {
-			path: '~/components/common',
-			global: true,
-			pathPrefix: false
-		  },
-		  '~/components'
-		]
-	  },
+				vite: {
+						css: {
+								devSourcemap: true
+						},
+						optimizeDeps: {
+								include: ['@nuxt-themes/docus'],
+								exclude: ['fsevents', 'globby', 'unicorn-magic']
+						},
+						build: {
+								chunkSizeWarningLimit: 1000,
+								cssCodeSplit: false,
+								minify: "terser",
+								terserOptions: {
+										compress: {
+												drop_console: true,
+												drop_debugger: true,
+										},
+								},
+						},
+						ssr: {
+								noExternal: [
+										'fsevents',
+										'globby',
+										'unicorn-magic',
+										'@nuxt/content',
+										'@nuxt-themes/docus',
+										'nuxt',
+										'#imports',
+										'#internal/nitro',
+										'#internal/nuxt'
+								]
+						}
+				},
+				components: {
+								dirs: [
+										{
+												path: '~/components/content',
+												global: true,
+												pathPrefix: false
+										},
+										{
+												path: '~/components/common',
+												global: true,
+												pathPrefix: false
+										},
+										'~/components'
+								]
+						},
 
 
-	nitro: {
-	  externals: {
-		inline: [
-		  'fsevents',
-		  'globby',
-		  'unicorn-magic',
-		  '@nuxt/content',
-		  '@nuxt-themes/docus'
-		]
-	  },
-	  prerender: {
-		crawlLinks: true,
-		routes: ["/"],
-	  },
-	  routeRules: {
-		"/**": { prerender: true },
-	  },
-	  minify: true,
-	  experimental: {
-		asyncContext: true,
-	  },
-	  compatibilityDate: '2024-04-03'
-	},
+				nitro: {
+						externals: {
+								inline: [
+										'fsevents',
+										'globby',
+										'unicorn-magic',
+										'@nuxt/content',
+										'@nuxt-themes/docus'
+								]
+						},
+						prerender: {
+								crawlLinks: true,
+								routes: ["/"],
+						},
+						routeRules: {
+								"/**": { prerender: true },
+						},
+						minify: true,
+						experimental: {
+								asyncContext: true,
+						},
+						compatibilityDate: '2024-04-03'
+				},
 
-	experimental: {
-	  payloadExtraction: true,
-	  renderJsonPayloads: true
-	},
+				experimental: {
+						payloadExtraction: true,
+						renderJsonPayloads: true
+				},
 
-	mdc: {
-	  headings: {
-		anchorLinks: {
-		  h1: false,
-		  h2: false,
-		  h3: false,
-		  h4: false,
-		  h5: false,
-		  h6: false
-		}
-	  }
-	},
+				mdc: {
+						headings: {
+								anchorLinks: {
+										h1: false,
+										h2: false,
+										h3: false,
+										h4: false,
+										h5: false,
+										h6: false
+								}
+						}
+				},
 
-	content: {
-	  documentDriven: true,
-	  highlight: {
-		theme: 'github-dark',
-		preload: [
-		  'bash',
-		  'json',
-		  'yaml',
-		  'powershell',
-		  'toml',
-		  'hcl',
-		  'terraform',
-		]
-	  },
-	  markdown: {
-		toc: {
-		  depth: 4,
-		  searchDepth: 4
-		},
-		remarkPlugins: {
-		  'remark-gfm': {
-			singleTilde: false
-		  }
-		}
-	  }
-	},
+				content: {
+						documentDriven: true,
+						highlight: {
+								theme: 'github-dark',
+								preload: [
+										'bash',
+										'json',
+										'yaml',
+										'powershell',
+										'toml',
+										'hcl',
+										'terraform',
+								]
+						},
+						markdown: {
+								toc: {
+										depth: 4,
+										searchDepth: 4
+								},
+								remarkPlugins: {
+										'remark-gfm': {
+												singleTilde: false
+										}
+								}
+						}
+				},
   })
