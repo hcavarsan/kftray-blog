84b4575b0c959f6a369f21ffa66026854606e788
hcavarsan <hcavarsan@yahoo.com.br>
2025-04-23 09:48:51 -0300

    feat: add i118n

--- plugins/umami.client.js
+++ plugins/umami.client.js
@@ -2,7 +2,7 @@
 export default defineNuxtPlugin(() => {
   const umamiScript = document.createElement('script')
   umamiScript.defer = true
-  umamiScript.src = 'https://analytics.cavarsa.app/script.js'
-  umamiScript.setAttribute('data-website-id', '14fdf04f-6284-42ce-a1fb-bee76923e10d')
+  umamiScript.src = 'https://umami.cavarsa.app/script.js'
+  umamiScript.setAttribute('data-website-id', '70662892-98e8-48ce-bde0-d360b7a0d0fc')
   document.head.appendChild(umamiScript)
 })
