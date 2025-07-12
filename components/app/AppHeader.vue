71a4a8561cfaf585be754ffc0674a6f1f0cbd849
hcavarsan <hcavarsan@yahoo.com.br>
2025-07-12 20:13:17 -0300

    feat: new blog post release

--- components/app/AppHeader.vue
+++ components/app/AppHeader.vue
@@ -9,9 +9,9 @@ const hasDialog = computed(() => navigation.value?.length > 1 || navigation.valu
 const currentBanner = computed(() => {
   const now = Date.now()
   return {
-    badgeText: "Release v0.19.0",
+    badgeText: "Release v0.20.0",
     text: "A new version of kftray has been released.",
-    link: "https://github.com/hcavarsan/kftray/releases/tag/v0.19.0",
+    link: "https://github.com/hcavarsan/kftray/releases/tag/v0.20.0",
     linkText: "Check it out →"
   }
 })
